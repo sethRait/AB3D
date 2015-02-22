f2b95e16319358a33bbd7ba7e927b80127be573f
janac meena <janac.meena@gmail.com>
2015-02-22 02:00:58 -0500

    Added code for displaying user information,

--- script.js
+++ script.js
@@ -7,32 +7,46 @@ function login(name, pass) {
   "use strict";
   console.log(name);
   console.log(pass);
-  /*ignore jslint start*/
   var db = new Firebase("https://assassins.firebaseio.com/");
-  /*ignore jslint end*/
+  
+  var user = db.child("users/"+name).on("value", function(snapshot){
+    if (snapshot.val()==null){
+      alert("Username or password doesn't exists");
+    }
+    else if (snapshot.val().passkey != pass){
+      alert("WRONG PASSWORD!!!!!!!!!!....or username");
+    }else{
+//      var table = document.getElementById('targets');
+      var tableContents="";
+      for (var key in snapshot.val().targets){
+        var val = snapshot.val().targets[key];
+        tableContents+="<tr><td>" + val + "</td></tr>";
+      }
+      $('#targets').append(tableContents);
+      $('#targets').append("<tr><td> Score:" + snapshot.val().score + "</td></tr>");
+    }
+  });
 }
+
 //Set sample data
-//function setTemplate() {
-//  /*ignore jslint start*/
-//  var db = new Firebase("https://assassins.firebaseio.com/");
-//  /*ignore jslint end*/
-//  var userTemplate = {
-//      "users": {
-//        user1: {
-//          name: "stapler",
-//          passkey: "123",
-//          score: 0,
-//          targets: {
-//            t1: "kat",
-//            t2: "jan",
-//            t3: "seth",
-//            t4: "david"
-//          }
-//        }
-//      }
-//  };
-//  db.set(userTemplate);
-//}
+function setTemplate() {
+  var db = new Firebase("https://assassins.firebaseio.com/");
+  var userTemplate = {
+      "users": {
+        "stapler" :{
+          passkey: "123",
+          score: 0,
+          targets: {
+            t1: "kat",
+            t2: "jan",
+            t3: "seth",
+            t4: "david"
+          }
+        }
+      }
+  };
+  db.set(userTemplate);
+}
 
 //function readFirst() {
 //  "use strict";
