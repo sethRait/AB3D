b6d81d24fea6c19d88956a874fc7649a2f24c654
admin <admin@web006225.wireless.mcgill.ca>
2015-02-22 02:04:05 -0500

    gotcha

---
+++ gotcha.js
@@ -0,0 +1,10 @@
+var user = form.elements["user"];
+var kill = form.elements["kill"];
+
+/*
+** Adds one point to user score. Removes kill from user target list and replaces it with a random target in the least-targetted pool.
+*/
+function randomized_newtarget(user, kill){
+		
+}
+
