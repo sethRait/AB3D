63e7e5dd650ff2a5bd6eea9ef3b039d7603f6da1
Katherine <cass@case.edu>
2015-02-21 22:35:44 -0500

    S'IL VOUS PLAIT

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
 /**
  * Created by Seth on 2/21/2015.
  */
+
+
+var database = {
+        var userProfiles: [undefined];
+
+//jquery goes here
+
+        randomTarget: function(){
+                return userProfiles[Math.random()];
+        }
+
+}
+
+var userProfile = {
+        name: undefined,
+        passkey: undefined,
+        score: 0;
+        targets: [undefined];
+
+        userProfile: function(name, passkey){
+                this.name = //
+                this.passkey = passkey;
+
+                for ( i = 0; i<3; i++) {
+                        this.targets = database.randomTarget();
+                }
+        }
+        updateName: function(s){
+                this.name= s;
+        }
+        updatePass: function(s){
+                this.passkey = s;
+        }
+
+        assasinate: function(d){
+                score++; //increment killer's score
+                this.targets[d].takeHit(); //decrement killee's score
+                this.targets[d] = database.randomTarget(); //generate a new target
+        }
+        takehit: function(){
+            if(this.score > 0) {
+                score--; //decrement score if hit
+            }
+        }
+        getTargets: function(){
+                var db = new Firebase("https://assassins.firebaseio.com/");
+                db.child("")
+                return targets;
+        }
+}
+
