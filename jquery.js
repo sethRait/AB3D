4afcbc9e97e34a11bea51894ea0e56fea83246cb
sethRait <sethrait95@gmail.com>
2015-02-22 03:04:46 -0500

    updated game description

--- jquery.js
+++ jquery.js
@@ -2,10 +2,26 @@
  * Created by Seth on 2/21/2015.
  */
 $(document).ready(function(){
+    var hidden1=true;
     $("#login").click(function(){
-        $("#hideme1").toggle().css("display: none");
+        if(hidden1) {
+            $("#hideme1").slideDown();
+            hidden1=false;
+        }
+        else{
+            $("#hideme1").slideUp();
+            hidden1=true;
+        }
     });
-    $("#newuser").click(function(){
-        $("#hideme2").toggle().css("display: none");
+    var hidden2=true;
+    $("#newuser").click(function() {
+        if (hidden2) {
+            $("#hideme2").slideDown();
+            hidden2 = false;
+        }
+        else {
+            $("#hideme2").slideUp();
+            hidden2 = true;
+        }
     });
 });
