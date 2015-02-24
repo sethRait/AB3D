93661efcd012a1dd4b3a52a7b12a8b2d3e3bbfa7
janac meena <janac.meena@gmail.com>
2015-02-24 15:39:00 -0500

    cosmetic

--- script.js
+++ script.js
@@ -42,37 +42,67 @@ function setTemplate() {
 }
 //-----------------------------------
 function createUser(name,pass){
+  playerName = name.value;
   alert(name.value);
   console.log(name.value);
   console.log(pass.value);
-  var user = db.child("users/"+name).on("value",  function(snapshot){
+      var db = new Firebase("https://assassins.firebaseio.com/");
+  
+//  if (db.child("users/"+name.value)!=null){
+//    alert("duplicate name");
+//  }else{
+//    alert("good!");
+//  }
+  var user = db.child("users/"+playerName).on("value", function(snapshot){
     if (snapshot.val() != null){
-      alert("please pick a different user name, u bitch");
+      alert("please pick a different user name");
     }else{
-      var db = new Firebase("https://assassins.firebaseio.com/users/"+name.value);
       var user = {
-              passkey: pass,
-              score: 0,
-              targets: {
-                t1: "random",
-                t2: "jan",
-                t3: "seth",
-                t4: "david"
+         playerName :{        
+          passkey: pass.value,
+          score: 0,
+          targets: {
+            t1: "random",
+            t2: "jan",
+            t3: "seth",
+            t4: "david"
           }
+        }
       }
       db.set(user);
     }
   });
 }
 
+function newuser(name, pass) {
+    var ref  = new Firebase("https://assassinate.firebaseio.com/");
+
+  ref.child("users/"+name.value).on("value", function(snapshot){
+      console.log(snapshot.val());
+      if (snapshot.val() != null){
+           alert("Sorry name already exists, please pick a new one");
+      }else{ 
+        ref = new Firebase("https://assassinate.firebaseio.com/users/"+name.value);
+        var usersRef = ref.set({
+            password: pass.value,
+            score: 0,
+            t1: name.value,
+            t2: name.value, 
+            t3: name.value,
+            t4: name.value
+        });
+      }
+    }
+);}
+
 //////////////////UserInfo.html///////////////
-function populatePlayerInfo(player){
-  var db = new Firebase("https://assassins.firebaseio.com/");
+//function populatePlayerInfo(player){
+//  var db = new Firebase("https://assassins.firebaseio.com/");
   
 //  var user = db.child("users/"+name.value).on("value", function(snapshot){
 //    for 
 //  }
-}
+//}
 
 
 
